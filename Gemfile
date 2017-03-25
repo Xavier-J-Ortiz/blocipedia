32c9ded71a12b43e9bfd0e315d2212110bfacd0f
xortiz <xavier.ortiz.ch@gmail.com>
2017-03-25 17:04:40 -0400

    added faker gem. Pretty cool for adding seed data.

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
  gem 'rails', '4.2.7'
  gem 'devise'
  gem 'pundit'
+ gem 'faker'
  
  group :production do
    gem 'pg'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+5.times do
+  User.create!(
+      email: Faker::Internet.email,
+      password: "1234567",
+      password_confirmation: "1234567",
+      confirmed_at: Time.now
+  )
+end
+
+users = User.all
+55.times do
+  Wiki.create!(
+      title: Faker::Hipster.sentence(3),
+      body: Faker::Hipster.paragraphs(3).join(' '),
+      user_id: users.sample.id,
+      private: false
+
+  )
+end
